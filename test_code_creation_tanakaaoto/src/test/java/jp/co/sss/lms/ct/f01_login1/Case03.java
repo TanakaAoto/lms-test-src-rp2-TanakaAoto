f06a04b7480c9c34469fc80e1cf2b54b3391a591
CRC-T019P <CRC-T019P@CRC-019P>
2026-03-24 15:14:29 +0900

    add:Case03.java 上部メニューの「ヘルプ」リンクからヘルプ画面に遷移 テストコード追加

--- test_code_creation_tanakaaoto/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
+++ test_code_creation_tanakaaoto/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
@@ -1,6 +1,9 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +12,10 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * 結合テスト ログイン機能①
@@ -35,14 +42,37 @@ public class Case03 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		// 指定のURLの画面を開く
+		webDriver.get("http://localhost:8080/lms");
+		// Title確認
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+		// エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		// フォーム要素の取得
+		WebElement loginId = webDriver.findElement(By.id("loginId"));
+		WebElement password = webDriver.findElement(By.id("password"));
+		WebElement submit = webDriver.findElement(By.className("btn"));
+		// フォーム入力
+		loginId.clear();
+		loginId.sendKeys("StudentAA01");
+		password.clear();
+		password.sendKeys("StudentAA00");
+		submit.click();
+		// 画面の遷移を待機
+		final WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(60));
+		wait.until(ExpectedConditions.titleIs("コース詳細 | LMS"));
+		// Title確認
+		assertEquals("コース詳細 | LMS", webDriver.getTitle());
+		// エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 }
